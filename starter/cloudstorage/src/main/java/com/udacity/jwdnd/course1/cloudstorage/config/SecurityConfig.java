05d0377ebfdbd557503c8c3705891c7b8184b915
Aziz, Chiara <chiara_aziz@optum.com>
2022-08-29 16:18:23 -0500

    added securityconfig stuff to security congif file

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
@@ -1,9 +1,16 @@
 package com.udacity.jwdnd.course1.cloudstorage.config;
 
+import com.udacity.jwdnd.course1.cloudstorage.service.AuthenticationService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private AuthenticationService authenticationService;
+
+    public SecurityConfig(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
 }
--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/User.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/User.java
@@ -1,4 +1,4 @@
-package com.udacity.jwdnd.course1.cloudstorage.modules;
+package com.udacity.jwdnd.course1.cloudstorage.model;
 
 public class User {
     private Integer userId;
