e1a77c340f4bc9b0055b1fc1c07f9f40baeb585b
Aziz, Chiara <chiara_aziz@optum.com>
2022-08-30 10:32:57 -0500

    authentication service and provider added

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/UserMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/UserMapper.java
@@ -5,14 +5,14 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
-import org.springframework.stereotype.Service;
+
 
 @Mapper
 public interface UserMapper {
     @Select("SELECT * FROM USERS WHERE username = #{username}")
     User getUser(String username);
 
-    @Insert("INSERT INTO USERS (username, password, firstName, lastName), VALUES(#{username}, #{password}, #{firstName}, #{lastName}")
+    @Insert("INSERT INTO USERS (username, password, firstName, lastName), VALUES(#{username}, #{password}, #{firstName}, #{lastName})")
     @Options(useGeneratedKeys = true, keyProperty = "userId")
     int insert(User user);
 }
