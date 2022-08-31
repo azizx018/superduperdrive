639b88c7a6a318ffe18c157d6d64263aa8a49c6f
Aziz, Chiara <chiara_aziz@optum.com>
2022-08-31 13:41:13 -0500

    added login controller

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -2,11 +2,16 @@ package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
+
+    public String getHomePage(Model model) {
+        return "home";
+    }
 
 
 }
