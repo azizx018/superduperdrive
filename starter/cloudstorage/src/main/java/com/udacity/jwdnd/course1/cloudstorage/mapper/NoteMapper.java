eb74cd358ce3c8aa78f4ea5216f9a9a7768301ec
Aziz, Chiara <chiara_aziz@optum.com>
2022-09-16 15:50:10 -0500

    added mapper for notes

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
@@ -1,4 +1,25 @@
 package com.udacity.jwdnd.course1.cloudstorage.mapper;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.Attachment;
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
 public interface NoteMapper {
+    @Select("SELECT * FROM NOTES WHERE noteid = #{noteId} AND userId = #{userId}")
+    Note getNoteInfoByNoteId(Integer noteId, Integer userId);
+
+
+    @Insert("INSERT INTO NOTES (noteId, notetitle, notedescription, userid) " +
+            "VALUES(#{noteId}, #{noteTitle}, #{noteDescription}, #{userId})")
+    @Options(useGeneratedKeys = true, keyProperty = "noteId")
+    Integer saveNote(Note note);
+
+    @Select("SELECT * FROM NOTES WHERE userId = ${userId}")
+    List<Note> getNoteByUserId(Integer userId);
+
+    @Delete("DELETE FROM NOTES WHERE noteId = ${noteId}")
+    int deleteNoteByNoteId(Integer noteId);
 }
