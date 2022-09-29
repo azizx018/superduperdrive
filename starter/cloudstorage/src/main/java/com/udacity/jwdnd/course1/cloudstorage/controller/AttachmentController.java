db36201123244c95e746798298ea3ea2ac9b6598
Aziz, Chiara <chiara_aziz@optum.com>
2022-09-29 14:02:15 -0500

    added note mappers for insert, select from note title and userid and delete note- files is still working

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/AttachmentController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/AttachmentController.java
@@ -5,18 +5,18 @@ import com.udacity.jwdnd.course1.cloudstorage.model.Attachment;
 import com.udacity.jwdnd.course1.cloudstorage.service.AttachmentListService;
 import com.udacity.jwdnd.course1.cloudstorage.service.UserService;
 
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -28,13 +28,15 @@ public class AttachmentController {
     private AttachmentListService attachmentListService;
     private UserService userService;
 
+    @Autowired
     public AttachmentController (AttachmentListService attachmentListService, UserService userService) {
         this.attachmentListService = attachmentListService;
         this.userService = userService;
     }
 
     @PostMapping("/upload/attachment")
-    public String uploadAttachment(@RequestParam("fileUpload") MultipartFile attachment, Authentication authentication, Model model) throws IOException{
+    //@RequestMapping(value = "/upload/attachment", method = RequestMethod.POST)
+    public  String uploadAttachment( @RequestParam("fileUpload") MultipartFile attachment, Authentication authentication, Model model) throws IOException{
         Integer currentUserId = userService.getUser(authentication.getName()).getUserId();
         String fileName = attachment.getOriginalFilename();
         Boolean isAttachmentNameAvailable = attachmentListService.isAttachmentNameAvailable(fileName, currentUserId);
--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
@@ -1,4 +1,20 @@
 package com.udacity.jwdnd.course1.cloudstorage.mapper;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import org.apache.ibatis.annotations.*;
+
+@Mapper
 public interface NoteMapper {
+    @Select("SELECT * FROM NOTES WHERE notetitle = #{notetitle} AND userId = #{userId}")
+    Note getNoteInfoByNoteTitle(String noteTile, Integer userId);
+
+    @Insert("INSERT INTO NOTES (noteId, notetitle, notedescription, userid) " +
+            "VALUES(#{noteId}, #{noteTitle}, #{noteDescription}, #{userId})")
+    @Options(useGeneratedKeys = true, keyProperty = "noteId")
+    Integer saveNote(Note note);
+
+    @Delete("DELETE FROM NOTES WHERE noteId = #{noteId}")
+    int deleteNoteByNoteId(Integer noteId);
+
+
 }
