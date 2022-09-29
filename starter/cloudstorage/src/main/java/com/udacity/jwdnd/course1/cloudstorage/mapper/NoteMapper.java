781c7bfe3433a2985c9d19c5f8252c412d491d78
Aziz, Chiara <chiara_aziz@optum.com>
2022-09-29 14:08:39 -0500

    added update note in mapper

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
@@ -8,13 +8,16 @@ public interface NoteMapper {
     @Select("SELECT * FROM NOTES WHERE notetitle = #{notetitle} AND userId = #{userId}")
     Note getNoteInfoByNoteTitle(String noteTile, Integer userId);
 
-    @Insert("INSERT INTO NOTES (noteId, notetitle, notedescription, userid) " +
+    @Insert("INSERT INTO NOTES (notetitle, notedescription, userid) " +
             "VALUES(#{noteId}, #{noteTitle}, #{noteDescription}, #{userId})")
     @Options(useGeneratedKeys = true, keyProperty = "noteId")
     Integer saveNote(Note note);
 
     @Delete("DELETE FROM NOTES WHERE noteId = #{noteId}")
     int deleteNoteByNoteId(Integer noteId);
+
+    @Update("UPDATE NOTES SET noteTitle = #{noteTitle}, #{noteDescription}, WHERE noteId = #{noteId}")
+    int updateNote(Note note);
 
 
 }
