330006ad829e8eed5611852ff49d0bc550df53d7
Aziz, Chiara <chiara_aziz@optum.com>
2022-09-29 14:12:20 -0500

    fleshed out note mapper

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
@@ -3,12 +3,17 @@ package com.udacity.jwdnd.course1.cloudstorage.mapper;
 import com.udacity.jwdnd.course1.cloudstorage.model.Note;
 import org.apache.ibatis.annotations.*;
 
+import java.util.List;
+
 @Mapper
 public interface NoteMapper {
-    @Select("SELECT * FROM NOTES WHERE notetitle = #{notetitle} AND userId = #{userId}")
-    Note getNoteInfoByNoteTitle(String noteTile, Integer userId);
+    @Select("SELECT * FROM NOTES WHERE userId = #{userId}")
+    List<Note> getNotesByUser(Integer userId);
 
-    @Insert("INSERT INTO NOTES (notetitle, notedescription, userid) " +
+    @Select("SELECT * FROM NOTES WHERE noteId = #{noteId}")
+    Note selectNote(Integer noteId);
+
+    @Insert("INSERT INTO NOTES (noteTitle, noteDescription, userId) " +
             "VALUES(#{noteId}, #{noteTitle}, #{noteDescription}, #{userId})")
     @Options(useGeneratedKeys = true, keyProperty = "noteId")
     Integer saveNote(Note note);
