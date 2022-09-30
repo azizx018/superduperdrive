250baa20fbe3e83bd4eda232084c7a9218ea6d60
Aziz, Chiara <chiara_aziz@optum.com>
2022-09-30 14:00:41 -0500

    finished credential controller

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/CredentialController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/CredentialController.java
@@ -5,6 +5,9 @@ import com.udacity.jwdnd.course1.cloudstorage.service.CredentialListService;
 import com.udacity.jwdnd.course1.cloudstorage.service.UserService;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -26,6 +29,18 @@ public class CredentialController {
             credentialListService.updateCred(credential);
         } else {
             credentialListService.saveCred(credential, currentUser);
+        }
+        return "redirect:/home";
+    }
+    @GetMapping("/deleteCredential/{credentialId}")
+    public String deleteCredential(@PathVariable("credentialId") Integer credentialId, Authentication authentication,
+                                    Model model) {
+        Integer currentUser = userService.getUser(authentication.getName()).getUserId();
+        try{
+            credentialListService.deleteCred(credentialId);
+        } catch (Exception e) {
+            model.addAttribute("error", true);
+            model.addAttribute("message", "There was an error deleting credential record " + credentialId);
         }
         return "redirect:/home";
     }
