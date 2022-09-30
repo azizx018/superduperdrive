ef31e956cc9e501ccb000c7670240e02fedcec81
Aziz, Chiara <chiara_aziz@optum.com>
2022-09-30 11:13:07 -0500

    delete note works

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/NotesController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/NotesController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.io.IOException;
@@ -34,6 +36,18 @@ public class NotesController {
         } else {
             noteListService.saveNote(note, currentUser);
 
+        }
+        return "redirect:/home";
+    }
+    @GetMapping("/deleteNote/{noteId}")
+    public String deleteNote(@PathVariable("noteId") Integer noteId, Authentication authentication, Model model) {
+        Integer currentUser = userService.getUser(authentication.getName()).getUserId();
+
+        try{
+            noteListService.deleteNote(noteId);
+        } catch (Exception e) {
+            model.addAttribute("error", true);
+            model.addAttribute("message", "There was a problem deleting note" + noteId);
         }
         return "redirect:/home";
     }
